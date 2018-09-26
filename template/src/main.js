c71b3660a60893b460dda3ba29fca2f3cf43a755
zhouxiao <juexro@163.com>
2018-09-26 16:36:26 +0800

    demo source

---
+++ template/src/main.js
@@ -0,0 +1,15 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './app'
+
+const isProduction = process.env.NODE_ENV !== 'production'
+Vue.config.debug = isProduction
+Vue.config.devtools = isProduction
+Vue.config.productionTip = isProduction
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#root',
+  router,
+  render: h => h(App)
+})

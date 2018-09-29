345e2b5bceaaf3ab44708a763ad352c90b1006bd
zhouxiao <juexro@163.com>
2018-09-29 16:13:14 +0800

    support hot reload

--- template/pages/pageA/main.js
+++ template/pages/pageA/main.js
@@ -3,4 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+render()
+
+if (module.hot) {
+  module.hot.accept(() => {
+    render()
+  })
+}

b2842a97ba7ba4ad1c02e0552b771e4520f30640
zhouxiao <juexro@163.com>
2018-09-29 13:52:30 +0800

    react demo

---
+++ template/pages/pageB/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import base from './base.css'
+class App extends Component {
+  render() {
+    return (
+      <div className={base.container}>
+        <h1>This is pageB.</h1>
+        <a href="/pageA">pageA</a>
+        <a href="/pageB">pageB</a>
+      </div>
+    )
+  }
+}
+
+export default App;
--- template/pages/pageB/main.js
+++ template/pages/pageB/main.js
@@ -1,14 +1,6 @@
 import '@babel/polyfill'
-import Vue from 'vue'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './app'
 
-const isProduction = process.env.NODE_ENV !== 'production'
-Vue.config.debug = isProduction
-Vue.config.devtools = isProduction
-Vue.config.productionTip = isProduction
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#root',
-  render: h => h(App)
-})
+ReactDOM.render(<App />, document.getElementById('root'))
